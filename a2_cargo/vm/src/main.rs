021849c5c17dccb5b283d39e978768e02fdbd86c
Greg Jenkins <gj585518@ohio.edu>
2019-02-26 15:37:31 -0500

    updated help.txt with notes from the second lab day

--- a2_cargo/vm/src/main.rs
+++ a2_cargo/vm/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use std::io;
+
+fn main() -> io::Result<()>{
+    let mut file_content = Vec::new();
+
+    let args: Vec<String> = env::args().collect();
+    let file = File::open(&args[1])?;
+    let file = BufReader::new(file);
+
+    for line in file.lines() {
+        
+    }
     println!("Hello, world!");
 }
